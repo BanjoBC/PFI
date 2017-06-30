7c3427c2cd4b692b3c092b24e2385de494a0c964
REClements <robert.e.clements@gmail.com>
2017-06-29 20:55:42 -0400

    exercise 8.1
    First try

---
+++ ex81.py
@@ -0,0 +1,14 @@
+#Write a function called chop that takes a list and modifies it, removing the first
+#and last elements, and returns None.
+#Then write a function called middle that takes a list and returns a new list that contains all but the first and last elements.
+
+letters = [a,b,c,d,e,f,g]
+
+def chop (a):
+	b = a.remove[0:1]
+	b = a.remove[len(a)-1:]
+	print(b)
+
+chop(letters)
+
+print(letters);
